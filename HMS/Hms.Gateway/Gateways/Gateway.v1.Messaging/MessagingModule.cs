ad3dc7eed0263af12adb76c0d2e2238cb02525d8
Jorge Armando <163047032+Jorge-Armando-Von-Doellinger@users.noreply.github.com>
2024-08-23 15:32:01 -0300

    update

--- HMS/Hms.Gateway/Gateways/Gateway.v1.Messaging/MessagingModule.cs
+++ HMS/Hms.Gateway/Gateways/Gateway.v1.Messaging/MessagingModule.cs
@@ -1,6 +1,8 @@
-﻿using Gateway.v1.Core.Messaging.Publisher;
+﻿using Gateway.v1.Core.Messaging.Listener;
+using Gateway.v1.Core.Messaging.Publisher;
 using Gateway.v1.Messaging.Configurator;
 using Gateway.v1.Messaging.Factory;
+using Gateway.v1.Messaging.Listener;
 using Gateway.v1.Messaging.Publisher;
 using Microsoft.Extensions.DependencyInjection;
 using Nuget.MessagingUtilities.MessageSettings;
@@ -14,13 +16,20 @@ namespace Gateway.v1.Messaging
             services
                 .AddFactories()
                 .AddConfigurators()
-                .AddPublishers();
+                .AddPublishers()
+                .AddListeners();
             return services;
         }
 
         public static IServiceCollection AddPublishers(this IServiceCollection services)
         {
             services.AddScoped<IMessagePublisher<IMessageSettings>, RabbitMqPublisher>();
+            return services;
+        }
+
+        public static IServiceCollection AddListeners(this IServiceCollection services)
+        {
+            services.AddScoped<IMessageListener, MessageListener>();
             return services;
         }
 
