81feeef5c00ad37cc95e1253e300c7fc59f5cf4a
jorge <jorgearmandovdj@gmail.com>
2024-12-22 17:21:37 -0300

    WIP on main

---
+++ HMS (Abandoned)/HMS.Nugets/Nugets/Nuget.Employee/Inputs/Update/ContractualInformationUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuget.Employee.Inputs.Update
+{
+    internal class ContractualInformationUpdate
+    {
+    }
+}
