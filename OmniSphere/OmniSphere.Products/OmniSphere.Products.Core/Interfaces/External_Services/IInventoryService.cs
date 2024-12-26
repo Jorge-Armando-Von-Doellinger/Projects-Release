3c598e947aca03fc43d5fd2019aaaef9dcfed751
jorge <jorgearmandovdj@gmail.com>
2024-12-26 18:21:39 -0300

    commit

--- OmniSphere/OmniSphere.Products/OmniSphere.Products.Core/Interfaces/External_Services/IInventoryService.cs
+++
@@ -1,9 +0,0 @@
-namespace OmniSphere.Products.Core.Interfaces.External_Services;
-
-public interface IInventoryService
-{
-    Task UpdateItemAsync();
-    Task AddItemAsync();
-    Task RemoveItemAsync();
-    Task UpdateStockAsync();
-}
